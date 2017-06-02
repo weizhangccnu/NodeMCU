5943a80c2503830869a9ecff01e1058762e48900
weizhangccnu <296448435@qq.com>
2017-06-02 11:50:04 +0800

    WiFi connected correctly

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,4 +1,6 @@
 #include <math.h>
+#include <ESP8266WiFi.h>
+
 #define uchar unsigned char
 #define LED 5
 uchar temp;             //define temperature parameter
@@ -9,11 +11,38 @@ int chr[40] = {0};      //define a array to store 40 bits
 unsigned int times;     //define times parameter
 uchar inByte = 10;      //receive data from python script
 
-void setup() {
-  Serial.begin(9600);               //Set Baud Rate
-  pinMode(LED, OUTPUT);             // Initialize the LED_BUILTIN pin as an output
-}
+//====================================================================//
+const char* ssid = "cbm706";                        //your network SSID (name)
+const char* password = "phy12345";                  //you network password
 
+const char* host = "data.sparkfun.com";             //host ip address
+const char* streamId   = "....................";
+const char* privateKey = "....................";
+
+int value = 0;
+//====================================================================//
+void setup() {
+  Serial.begin(9600);                   //Set Baud Rate
+  pinMode(LED, OUTPUT);                 // Initialize the LED_BUILTIN pin as an output
+
+//  Serial.println();                   //we start by connecting to a WiFi network
+//  Serial.println();     
+  Serial.print("Connecting to ");
+
+  WiFi.begin(ssid,password);
+
+  while(WiFi.status() != WL_CONNECTED)
+  {
+      delay(500);
+      Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+}
 // the loop function runs over and over again forever
 void loop() {
   if( Serial.available() > 0)               //receive buffer data more than zero
@@ -22,6 +51,27 @@ void loop() {
       Serial.write(inByte);
       delay(100);
   }
+//====================================================================//
+//WiFi connection section
+//====================================================================// 
+  delay(1000);
+  ++value;
+  Serial.print("Connecting to ");
+  Serial.println(host);
+  WiFiClient client;                            //Use WiFiClient class to creat TCP connections
+  const int httpPort = 80;
+  if(!client.connect(host, httpPort))
+  {
+    Serial.println("Connection failed");
+    return;  
+  }
+//  String url = "/input/";                       //Create a URI for the request
+//  url += streamId;
+//  url += "?private_key=";
+//  url += 
+//====================================================================//
+//DHT11 fetch data code section
+//====================================================================// 
       bgn:                                  //define a label
       delay(200);
       pinMode(LED, OUTPUT);                 //output mode
@@ -66,6 +116,10 @@ void loop() {
       }
       humi = chr[0]*128+chr[1]*64+chr[2]*32+chr[3]*16+chr[4]*8+chr[5]*4+chr[6]*2+chr[7];
       temp = chr[16]*128+chr[17]*64+chr[18]*32+chr[19]*16+chr[20]*8+chr[21]*4+chr[22]*2+chr[23];
-      Serial.write(temp);                   //return temperature data to python script
-      Serial.write(humi);                   //return humidity data to python script
+      Serial.print("Temperature:");
+      Serial.print(temp);                   //return temperature data to python script
+      Serial.print(' ');
+      Serial.print("Humidity:");
+      Serial.print(humi);                   //return humidity data to python script
+      Serial.print('\n');
 }
